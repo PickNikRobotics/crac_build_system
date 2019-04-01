36e57f6f6f97ccbc72e9cf02d0ac5177dc6ab4fe
Mike Lautman <mikeblautman@github.com>
2019-04-01 10:55:33 -0600

    WIP: tmp: backup backup 2 final final done working version.

---
+++ src/super_important_script_backup.sh
@@ -0,0 +1,29 @@
+# Ridiculous stuff
+
+function selfdestruct
+{
+    seconds=10; date1=$((`date +%s` + $seconds));
+    while [ "$date1" -ne `date +%s` ]; do
+    echo -ne "Self Destruct In $(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r";
+    done
+    echo ""
+    exit 1
+}
+
+# Show fake hacker script
+alias hacker='hexdump -C /dev/urandom | grep "fd b4"'
+
+function downloadRAM
+{
+    i=1
+    sp="/-\|"
+    echo -n ' '
+    read -p "How much RAM do you want to pirate? [8gb]"
+    echo -n 'Downloading '
+    seconds=5; date1=$((`date +%s` + $seconds));
+    while [ "$date1" -ne `date +%s` ]; do
+        printf "\b${sp:i++%${#sp}:1}"
+    done
+    printf "\b"
+    echo  "Done"
+}
